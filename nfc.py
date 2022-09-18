909f8dc3e4095b39daa9494648e46aa4c70f017e
bill <billtao@pku.edu.cn>
2022-09-18 08:32:18 -0500

    Refine grid, loop over all water data

--- nfc.py
+++ nfc.py
@@ -3,14 +3,16 @@
 """Helper functions for NFC.
 """
 
+import argparse
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import struct
+from scipy.interpolate import RectBivariateSpline
+import scipy as sp
 import os
 import numpy as np
 np.set_printoptions(precision=4)
-import scipy as sp
-import struct
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-import argparse
+
 
 def read_complex_binary2(filename):
     """ Read file of float32 into complex array.
@@ -18,9 +20,10 @@ def read_complex_binary2(filename):
 
     with open(filename, 'rb') as f:
         bytes = f.read()
-    data = np.frombuffer(bytes, dtype=np.float32).reshape(-1,2)
-    data = data[:,0] + 1j*data[:,1]
+    data = np.frombuffer(bytes, dtype=np.float32).reshape(-1, 2)
+    data = data[:, 0] + 1j*data[:, 1]
     return data
+
 
 def get_rssis(filename):
     """ Get RSSI time series from file.
@@ -35,48 +38,55 @@ def get_rssis(filename):
     rssis = np.array(rssis)
     return rssis
 
+
 def get_model(dirname, prefix, n):
     """ Get the likelihood model from directory of data files.
     """
-
+    n += 1  # Grid is 0 to n
     f_mean = np.zeros((n, n))
-    f_dev  = np.zeros((n, n))
-    
+    f_dev = np.zeros((n, n))
+
     for x in tqdm(range(n)):
         for y in range(n):
             filename = os.path.join(dirname, f"{prefix}{x:>02}{y:>02}")
             rssis = get_rssis(filename)
 
-            f_mean[x,y] = np.mean(rssis)
-            f_dev[x,y]  = np.std(rssis)
+            f_mean[x, y] = np.mean(rssis)
+            f_dev[x, y] = np.std(rssis)
 
-    return f_mean, f_dev
+    return RectBivariateSpline(range(n), range(n), f_mean), RectBivariateSpline(range(n), range(n), f_dev), f_mean, f_dev
 
 
-def calc_likelihood(rssis, x, y, f_mean, f_dev, c = 0):
+def log_normal_pdf(x, mean, stddev):
+    """ Compute log of normal pdf.
+    """
+    return -0.5*np.log(2*np.pi*stddev**2) - (x-mean)**2/(2*stddev**2)
+
+
+def calc_log_likelihood(rssis, x, y, f_mean, f_dev, c=0, n=0):
     """ Compute likelihood of observing 4 rssis measurements given the ground truth
         location is x, y for a model given by f_mean, f_dev.
     """
-    n = f_mean.shape[0]
 
-    likelihood = 1.0
-    likelihood *= sp.stats.norm.pdf(rssis[0], f_mean[x,y]         + c, f_dev[x,y])
-    likelihood *= sp.stats.norm.pdf(rssis[1], f_mean[n-1-y,x]     + c, f_dev[n-1-y,x])
-    likelihood *= sp.stats.norm.pdf(rssis[2], f_mean[n-1-x,n-1-y] + c, f_dev[n-1-x,n-1-y])
-    likelihood *= sp.stats.norm.pdf(rssis[3], f_mean[y,n-1-x]       + c, f_dev[y,n-1-x])
+    log_likelihood = 0.0
+    log_likelihood += log_normal_pdf(rssis[0], f_mean(x, y) + c, f_dev(x, y))
+    log_likelihood += log_normal_pdf(rssis[1],
+                                     f_mean(n-y, x) + c, f_dev(n-y, x))
+    log_likelihood += log_normal_pdf(rssis[2],
+                                     f_mean(n-x, n-1-y) + c, f_dev(n-x, n-y))
+    log_likelihood += log_normal_pdf(rssis[3],
+                                     f_mean(y, n-x) + c, f_dev(y, n-x))
 
-    return likelihood
+    return log_likelihood
 
-def localize_mle(rssis, f_mean, f_dev, c = 0):
+
+def localize_mle(rssis, f_mean, f_dev, c=0, n=0):
     """ Compute location estimate using maximum-likelihood estimation.
     """
-    n = f_mean.shape[0]
 
-    likelihood_grid = np.ones((n,n))
-
-    for x in range(n):
-        for y in range(n):
-            likelihood_grid[x,y] = calc_likelihood(rssis, x, y, f_mean, f_dev, c)
+    likelihood_grid = np.ones((n, n))
+    likelihood_grid = np.vectorize(calc_log_likelihood, excluded=['rssis', 'f_mean', 'f_dev', 'n'])(
+        rssis=rssis, x=np.arange(0, n, 0.1), y=np.arange(0, n, 0.1)[:, np.newaxis], f_mean=f_mean, f_dev=f_dev, c=c, n=n)
 
     return np.unravel_index(np.argmax(likelihood_grid), likelihood_grid.shape)
 
@@ -85,24 +95,35 @@ if __name__ == '__main__':
 
     # Parse input arguments.
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dirname',   required=True, help="Path to directory of data files.")
-    parser.add_argument('--prefix',    type=int, required=True, help="For each condition (air, water, oil, etc).")
-    parser.add_argument('--grid_size', type=int, required=True, help="Grid size as an integer.")
-    parser.add_argument('--query',     type=float, required=True, nargs=4, help="RSSI observation to localize.")
+    parser.add_argument('--dirname',   required=True,
+                        help="Path to directory of data files.")
+    parser.add_argument('--prefix',    type=int, required=True,
+                        help="For each condition (air, water, oil, etc).")
+    parser.add_argument('--grid_size', type=int,
+                        required=True, help="Grid size as an integer.")
+    parser.add_argument('--query',     type=float, required=True,
+                        nargs=4, help="RSSI observation to localize.")
     args = parser.parse_args()
 
     # Compute the model from the directory.
-    f_mean, f_dev = get_model(args.dirname, args.prefix, args.grid_size)
-
-    # Visualize the model.
-    x, y = np.meshgrid(range(f_mean.shape[0]), range(f_mean.shape[1]))
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(x, y, f_mean)
-    plt.title('f_mean vs position')
-    plt.show()
-
-    # Output MLE location for random RSSI observation.
-    location = localize_mle(args.query, f_mean, f_dev)
-    print(location)
-    
+    f_mean, f_dev, f_mean_points, f_dev_points = get_model(
+        args.dirname, 0, args.grid_size)
+    n = args.grid_size  # grid is 0 to n (n+1 by n+1)
+    # Create testing grid.
+    test_rssis = np.empty((n+1, n+1))
+    for x in tqdm(range(n+1)):
+        for y in range(n+1):
+            filename = os.path.join(args.dirname, f"1{x:>02}{y:>02}")
+            rssis = get_rssis(filename)
+            test_rssis[x, y] = np.mean(rssis)
+    # Start testing
+    errors = []
+    for x in tqdm(range(args.grid_size)):
+        for y in range(args.grid_size):
+            rssis = [test_rssis[x, y], test_rssis[n-y, x],
+                     test_rssis[n-x, n-y], test_rssis[y, n-x]]
+            x0, y0 = localize_mle(rssis, f_mean, f_dev, range(-10,10), args.grid_size)
+            x0/=10
+            y0/=10
+            errors.append(np.sqrt((x-x0)**2+(y-y0)**2))
+    print(np.mean(errors))
